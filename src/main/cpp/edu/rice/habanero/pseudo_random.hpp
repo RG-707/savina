781c700ca935b5f9926e2d6320d1517c844dadd8
Hauke Goldhammer <hauke.goldhammer@haw-hamburg.de>
2019-04-25 18:50:49 +0200

    -- add next_boolean and next_gaussian to pseudo_random

--- src/main/cpp/edu/rice/habanero/pseudo_random.hpp
+++ src/main/cpp/edu/rice/habanero/pseudo_random.hpp
@@ -1,14 +1,14 @@
 #ifndef PSEUDO_RANDOM_HPP
 #define PSEUDO_RANDOM_HPP
 
-#include <vector>
+#include <cmath>
 #include <limits>
 #include <random>
+#include <vector>
 
 class pseudo_random {
 public:
-  pseudo_random(long seed)
-      : value_(seed) {
+  pseudo_random(long seed) : value_(seed) {
     // nop
   }
 
@@ -32,10 +32,37 @@ public:
   }
 
   double next_double() {
-    return 1.0 / (next_long() +1);
+    return 1.0 / (next_long() + 1);
+  }
+
+  bool next_boolean() {
+    return next_int(2) != 0;
+  }
+
+  /// Pseudo next gaussian implemented after
+  /// https://docs.oracle.com/javase/7/docs/api/java/util/Random.html
+  // TODO add java pseudo implementation
+  double next_gaussian() {
+    if (have_next_next_gaussian) {
+      have_next_next_gaussian = false;
+      return next_next_gaussian;
+    } else {
+      double v1, v2, s;
+      do {
+        v1 = 2 * next_double() - 1; // between -1.0 and 1.0
+        v2 = 2 * next_double() - 1; // between -1.0 and 1.0
+        s = v1 * v1 + v2 * v2;
+      } while (s >= 1 || s == 0);
+      double multiplier = sqrt(-2 * log(s) / s);
+      next_next_gaussian = v2 * multiplier;
+      have_next_next_gaussian = true;
+      return v1 * multiplier;
+    }
   }
 
 private:
-  long value_ = 74755; 
+  long value_ = 74755;
+  double next_next_gaussian = 0;
+  bool have_next_next_gaussian = false;
 };
 #endif // PSEUDO_RANDOM_HPP
